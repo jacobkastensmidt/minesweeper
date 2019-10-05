17fa65f69dbf68ac9093a31659627e55f1684562
Jacob Kastensmidt <jksmidt@gmail.com>
2019-10-05 19:13:15 -0400

    Adding tile toggling

--- src/Board/Tile.js
+++ src/Board/Tile.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBomb, faFlag } from '@fortawesome/free-solid-svg-icons'
 import './Tile.css';
 
 const symbols = {
-    bomb: 'fa-bomb',
-    flag: 'fa-flag',
-    blank: 'blank'
+    bomb: faBomb,
+    flag: faFlag,
+    blank: null
 };
 
-export default class Tile extends Component {
+class Tile extends Component {
+    state = {
+        isRevealed: false,
+        isBomb: true,
+        isFlagged: false,
+        symbol: null
+    }
+
+    handleClick = () => {
+        if (!this.state.isRevealed && !this.state.isFlagged) {
+            this.setState(
+                {isRevealed: true},
+                () => {
+                    if (this.state.isBomb) {
+                        this.setState({symbol: symbols.bomb});
+                    }
+                });
+        }
+    }
+    
+    handleContextMenu = (e) => {
+        e.preventDefault();
+        this.setState(state => {
+            return {isFlagged: !state.isFlagged}
+        }, () => {
+            if(!this.state.isRevealed) {
+                if(this.state.isFlagged) {
+                    this.setState({symbol: symbols.flag});
+                } else if (!this.state.isFlagged) {
+                    this.setState({symbol: symbols.blank});
+                }
+            }
+        });
+    }
+
     render() {
         return (
-            <div className="Tile">
-                <i className={'icon fas ' + symbols.blank}></i>
+            <div
+                className={`Tile${(this.state.isRevealed) ? ' depressed' : ''}`}
+                onClick={this.handleClick}
+                onContextMenu={this.handleContextMenu}
+            >
+                {this.state.symbol && ((this.state.isFlagged && !this.state.isRevealed) ||
+                    (this.state.isBomb && this.state.isRevealed)) &&
+                    <FontAwesomeIcon icon={this.state.symbol} />
+                }
             </div>
         );
     }
 }
+
+export default Tile;
