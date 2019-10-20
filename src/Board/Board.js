62263d6bee03603a0f46679d4a511827a40dcadd
Jacob Kastensmidt <jksmidt@gmail.com>
2019-10-20 17:27:29 -0400

    More responsive CSS and board gen start

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -3,23 +3,36 @@ import Tile from './Tile';
 import './Board.css';
 
 export default class Board extends Component {
-    render() {
-        const {boardWidth} = this.props;
-        let {boardHeight} = this.props;
-        boardHeight = boardHeight||boardWidth;
-        const boardSize = Array.from(new Array(boardWidth * boardHeight), (val,index)=>index);
-        const boardGridStyle = {
-            'gridTemplateColumns': `repeat(${boardWidth}, auto)`
-        };
+	determineNumberOfMines(height, width) {
+		const max = (height - 1) * (width - 1);
+		const min = (height > width) ? height : width;
+		let threeRandom = Array.from(Array(3), (value, key) => Math.random() * (max - min) + min);
+		return Math.floor(threeRandom.reduce(
+      (accumulator, current) => accumulator + current,
+      0)
+      / threeRandom.length
+    );
+	}
 
-        return (
-            <div className="Board">
-                <div className="BoardGrid" style={boardGridStyle}>
-                    {boardSize.map((value, index) => {
-                        return <Tile key={value} boardWidth={boardWidth} />
-                    })}
-                </div>
-            </div>
-        )
-    }
+	render() {
+		const {boardWidth} = this.props;
+		let {boardHeight} = this.props;
+		boardHeight = boardHeight||boardWidth;
+		const boardSize = Array(boardWidth * boardHeight).fill(false);
+		const boardGridStyle = {
+			'gridTemplateColumns': `repeat(${boardWidth}, auto)`
+		};
+		let a = this.determineNumberOfMines(boardHeight, boardWidth);
+		console.log(boardHeight + " " + boardWidth + " " + a);
+
+		return (
+			<div className="Board">
+				<div className="BoardGrid" style={boardGridStyle}>
+					{boardSize.map((value, index) => {
+						return <Tile key={index} isBomb={true}  />
+					})}
+				</div>
+			</div>
+		)
+	}
 }
--- src/Board/Tile.js
+++ src/Board/Tile.js
@@ -4,62 +4,63 @@ import { faBomb, faFlag } from '@fortawesome/free-solid-svg-icons'
 import './Tile.css';
 
 const symbols = {
-    bomb: faBomb,
-    flag: faFlag,
-    blank: null
+  bomb: faBomb,
+  flag: faFlag,
+  blank: null
 };
 
 export default class Tile extends Component {
-    state = {
-        isRevealed: false,
-        isBomb: true,
-        isFlagged: false,
-        symbol: null
+  state = {
+      isRevealed: false,
+      isBomb: this.props.isBomb||false,
+      isFlagged: false,
+      symbol: null
     }
 
-    handleClick = () => {
-        if (!this.state.isRevealed && !this.state.isFlagged) {
-            this.setState(
-                {isRevealed: true},
-                () => {
-                    if (this.state.isBomb) {
-                        this.setState({symbol: symbols.bomb});
-                    }
-                });
-        }
-    }
-    
-    handleContextMenu = (e) => {
-        e.preventDefault();
-        this.setState(state => {
-            return {isFlagged: !state.isFlagged}
-        }, () => {
-            if(!this.state.isRevealed) {
-                if(this.state.isFlagged) {
-                    this.setState({symbol: symbols.flag});
-                } else if (!this.state.isFlagged) {
-                    this.setState({symbol: symbols.blank});
-                }
-            }
+  handleClick = () => {
+    if (!this.state.isRevealed && !this.state.isFlagged) {
+      this.setState(
+        {isRevealed: true},
+        () => {
+          if (this.state.isBomb) {
+            this.setState({symbol: symbols.bomb});
+          }
         });
     }
-
-    render() {
-        const style = {
-            position: 'absolute',
-            'font-size': '1.5em'
+  }
+    
+  handleContextMenu = (e) => {
+      e.preventDefault();
+      this.setState(state => {
+        return {isFlagged: !state.isFlagged}
+      }, () => {
+        if(!this.state.isRevealed) {
+          if(this.state.isFlagged) {
+            this.setState({symbol: symbols.flag});
+          } else if (!this.state.isFlagged) {
+            this.setState({symbol: symbols.blank});
+          }
         }
-        return (
-            <div
-                className={`Tile${(this.state.isRevealed) ? ' depressed' : ''}`}
-                onClick={this.handleClick}
-                onContextMenu={this.handleContextMenu}
-            >
-                {this.state.symbol && ((this.state.isFlagged && !this.state.isRevealed) ||
-                    (this.state.isBomb && this.state.isRevealed)) &&
-                    <FontAwesomeIcon icon={this.state.symbol} style={style} />
-                }
-            </div>
-        );
+      });
+  }
+
+  render() {
+    const style = {
+      position: 'absolute',
+      width: '100%',
+      height: '100%'
     }
+    return (
+      <div
+        className={`Tile${(this.state.isRevealed) ? ' depressed' : ''}`}
+        onClick={this.handleClick}
+        onContextMenu={this.handleContextMenu}
+      >
+        {this.state.symbol && ((this.state.isFlagged && !this.state.isRevealed) ||
+          (this.state.isBomb && this.state.isRevealed)) &&
+          <FontAwesomeIcon icon={this.state.symbol} style={style} viewBox="-200 -200 912 912"/>
+        }
+      </div>
+    );
+  }
 }
