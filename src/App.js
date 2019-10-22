c2598495ac07b5aae40dfd80e85c33e0bcb42a59
Jacob Kastensmidt <jksmidt@gmail.com>
2019-10-21 23:55:05 -0400

    Adjacent mine numbers Tiles tracking how many adjacent mines there are Tiles displaying the number of adjacent mines when revealed

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import Board from './Board/Board';
 
 function App() {
-  const width = 5;
+  const width = 10;
   return (
     <div className="App">
-      <Board width={width} />
+      <Board width={width} numberOfMines={10}/>
     </div>
   );
 }
--- src/Board/Tile.js
+++ src/Board/Tile.js
@@ -4,26 +4,48 @@ import { faBomb, faFlag } from '@fortawesome/free-solid-svg-icons'
 import './Tile.css';
 
 const symbols = {
-  bomb: faBomb,
+  mine: faBomb,
   flag: faFlag,
   blank: null
 };
 
+function Symbol(props) {
+  const {isMine, isFlagged, isRevealed, numberOfAdjacentMines, symbol} = props.tileState;
+  const style = {
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  };
+  const viewBoxDimensions = "-200 -200 912 912";
+  if (symbol && ((isFlagged && !isRevealed) || (isMine && isRevealed))) {
+    return <FontAwesomeIcon icon={symbol} style={style} viewBox={viewBoxDimensions}/>;
+  } else if (!isMine && isRevealed && numberOfAdjacentMines > 0) {
+    return (
+      <svg viewBox="-20 -85 100 100" xmlns="http://www.w3.org/2000/svg" style={style}>
+        <text fontSize="100">{numberOfAdjacentMines}</text>
+      </svg>
+    )
+  }
+  return null;
+}
+
 export default class Tile extends Component {
   state = {
-      isRevealed: false,
-      isBomb: this.props.isBomb||false,
-      isFlagged: false,
-      symbol: null
-    };
+    isBlank: this.props.tileData.isBlank,
+    isMine: this.props.tileData.isMine,
+    isFlagged: false,
+    isRevealed: false,
+    numberOfAdjacentMines: this.props.tileData.numberOfAdjacentMines,
+    symbol: null
+  };
 
   handleClick = () => {
     if (!this.state.isRevealed && !this.state.isFlagged) {
       this.setState(
         {isRevealed: true},
         () => {
-          if (this.state.isBomb) {
-            this.setState({symbol: symbols.bomb});
+          if (this.state.isMine) {
+            this.setState({symbol: symbols.mine});
           }
         });
     }
@@ -45,21 +67,14 @@ export default class Tile extends Component {
   }
 
   render() {
-    const style = {
-      position: 'absolute',
-      width: '100%',
-      height: '100%'
-    }
+    const {isRevealed} = this.state;
     return (
       <div
-        className={`Tile${(this.state.isRevealed) ? ' depressed' : ''}`}
+        className={`Tile${(isRevealed) ? ' depressed' : ''}`}
         onClick={this.handleClick}
         onContextMenu={this.handleContextMenu}
       >
-        {this.state.symbol && ((this.state.isFlagged && !this.state.isRevealed) ||
-          (this.state.isBomb && this.state.isRevealed)) &&
-          <FontAwesomeIcon icon={this.state.symbol} style={style} viewBox="-200 -200 912 912"/>
-        }
+        <Symbol tileState={this.state} />
       </div>
     );
   }
