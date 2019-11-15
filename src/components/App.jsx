c85f3ea5780555cb57294ae4dcecce5eb517997c
Jacob Kastensmidt <jksmidt@gmail.com>
2019-11-14 20:18:24 -0500

    Play again button functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      gameId: 0,
       width: 10,
       customNumberOfMines: 10,
       isGameover: false,
@@ -24,8 +25,17 @@ export default class App extends Component {
     });
   }
 
+  resetGame = () => {
+    const { gameId } = this.state;
+    this.setState({
+      gameId: gameId + 1,
+      isGameover: false,
+      isWin: false
+    });
+  }
+
   render = () => {
-    const { width, customNumberOfMines, isGameover, isWin } = this.state;
+    const { width, customNumberOfMines, isGameover, isWin, gameId } = this.state;
     const gameOverMessage = isWin ? "You win!" : "You lose!";
     const displayGameover = isGameover ? "flex-center" : "hide";
     return (
@@ -37,12 +47,13 @@ export default class App extends Component {
               <div id="gameover-prompt">
                 <div id="game-over-message">{gameOverMessage}</div>
                 <div className="button-group">
-                  <button type="button">Play again?</button>
+                  <button type="button" onClick={this.resetGame}>Play again?</button>
                   <button type="button">Options</button>
                 </div>
               </div>
             </div>
             <Board
+              key={gameId}
               width={width}
               customNumberOfMines={customNumberOfMines}
               triggerGameover={this.triggerGameover}
